7e61c4a2f9618d0a712119201d7c0743db863887
Théo6704 <loltheojeanthomas@gmail.com>
2022-03-24 22:28:54 +0100

    formulaire mot de passe

--- src/Connexion.js
+++ src/Connexion.js
@@ -27,3 +27,20 @@ function Favs_Counter() {
       counterCryptocurrency.innerHTML += `/${buttonsCryptocurrency.length} sélectionné`
    }
 }
+
+const eye = document.querySelector(".feather-eye");
+const eyeoff = document.querySelector(".feather-eye-off");
+const passwordField = document.querySelector("input[type=password]");
+
+eye.addEventListener("click", () => {
+   eye.style.display = "none";
+   eyeoff.style.display = "block";
+   passwordField.type = "text";
+});
+ 
+eyeoff.addEventListener("click", () => {
+   eyeoff.style.display = "none";
+   eye.style.display = "block";
+   passwordField.type = "password";
+});
+ 
